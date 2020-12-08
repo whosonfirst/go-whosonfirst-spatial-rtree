15cad5cba49f044068b0050d44d2f735fc4816e6
thisisaaronland <devnull@localhost>
2020-12-08 12:07:58 -0500

    Makefile, tools

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # go-whosonfirst-spatial-rtree
+
+## Tools
+
+```
+$> make cli
+```
+
+### query
+
+## See also
+
+* https://github.com/whosonfirst/go-whosonfirst-spatial
+* https://github.com/dhconnelly/rtreego
---
+++ cmd/query/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	_ "github.com/whosonfirst/go-whosonfirst-spatial-rtree"
+	"github.com/whosonfirst/go-whosonfirst-spatial/database"
+	"github.com/whosonfirst/go-whosonfirst-spatial/filter"
+	"github.com/whosonfirst/go-whosonfirst-spatial/geo"
+	"github.com/whosonfirst/go-whosonfirst-spr"
+	"log"
+)
+
+func main() {
+
+	database_uri := flag.String("database-uri", "", "...")
+	latitude := flag.Float64("latitude", 0.0, "...")
+	longitude := flag.Float64("longitude", 0.0, "...")
+
+	flag.Parse()
+
+	ctx := context.Background()
+	db, err := database.NewSpatialDatabase(ctx, *database_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to create database for '%s', %v", *database_uri, err)
+	}
+
+	c, err := geo.NewCoordinate(*longitude, *latitude)
+
+	if err != nil {
+		log.Fatalf("Failed to create new coordinate, %v", err)
+	}
+
+	f, err := filter.NewSPRFilter()
+
+	if err != nil {
+		log.Fatalf("Failed to create SPR filter, %v", err)
+	}
+
+	r, err := db.PointInPolygon(ctx, c, f)
+
+	if err != nil {
+		log.Fatalf("Failed to query database with coord %v, %v", c, err)
+	}
+
+	enc, err := json.Marshal(r)
+
+	if err != nil {
+		log.Fatalf("Failed to marshal results, %v", err)
+	}
+
+	fmt.Println(string(enc))
+}
