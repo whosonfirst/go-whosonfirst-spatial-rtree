3c23023e8cd3efa47360273311f83d72de487acc
thisisaaronland <devnull@localhost>
2020-12-18 17:04:33 -0800

    snapshot

--- cmd/query/main.go
+++ cmd/query/main.go
@@ -33,38 +33,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	flags.AppendQueryFlags(fs)
+	err = flags.AppendQueryFlags(fs)
 
-	/*
-		latitude := fs.Float64("latitude", 0.0, "A valid latitude.")
-		longitude := fs.Float64("longitude", 0.0, "A valid longitude.")
-
-		geometries := fs.String("geometries", "all", "Valid options are: all, alt, default.")
-
-		var props multi.MultiString
-		fs.Var(&props, "properties", "One or more Who's On First properties to append to each result.")
-
-		var pts multi.MultiString
-		fs.Var(&pts, "placetype", "One or more place types to filter results by.")
-
-		var alt_geoms multi.MultiString
-		fs.Var(&alt_geoms, "alternate-geometry", "One or more alternate geometry labels (wof:alt_label) values to filter results by.")
-
-		var is_current multi.MultiString
-		fs.Var(&is_current, "is-current", "One or more existential flags (-1, 0, 1) to filter results by.")
-
-		var is_ceased multi.MultiString
-		fs.Var(&is_ceased, "is-ceased", "One or more existential flags (-1, 0, 1) to filter results by.")
-
-		var is_deprecated multi.MultiString
-		fs.Var(&is_deprecated, "is-deprecated", "One or more existential flags (-1, 0, 1) to filter results by.")
-
-		var is_superseded multi.MultiString
-		fs.Var(&is_superseded, "is-superseded", "One or more existential flags (-1, 0, 1) to filter results by.")
-
-		var is_superseding multi.MultiString
-		fs.Var(&is_superseding, "is-superseding", "One or more existential flags (-1, 0, 1) to filter results by.")
-	*/
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	flags.Parse(fs)
 
@@ -92,7 +65,7 @@ func main() {
 	mode, _ := flags.StringVar(fs, "mode")
 
 	latitude, _ := flags.Float64Var(fs, "latitude")
-	longitude, _ := flags.Float64Var(fs, "latitude")
+	longitude, _ := flags.Float64Var(fs, "longitude")
 
 	geometries, _ := flags.StringVar(fs, "geometries")
 
