e54e02434fc87397a4c664674ec1e448ec3eb777
thisisaaronland <devnull@localhost>
2020-12-18 17:21:35 -0800

    update to use flags.NewSPRFilterFromFlagSet()

--- cmd/query/main.go
+++ cmd/query/main.go
@@ -4,19 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	// "github.com/sfomuseum/go-flags/multi"
 	"github.com/whosonfirst/go-whosonfirst-geojson-v2/feature"
 	"github.com/whosonfirst/go-whosonfirst-geojson-v2/properties/geometry"
 	"github.com/whosonfirst/go-whosonfirst-index"
 	_ "github.com/whosonfirst/go-whosonfirst-index/fs"
 	_ "github.com/whosonfirst/go-whosonfirst-spatial-rtree"
 	"github.com/whosonfirst/go-whosonfirst-spatial/database"
-	"github.com/whosonfirst/go-whosonfirst-spatial/filter"
 	"github.com/whosonfirst/go-whosonfirst-spatial/flags"
 	"github.com/whosonfirst/go-whosonfirst-spatial/geo"
 	"io"
 	"log"
-	"net/url"
 )
 
 func main() {
@@ -67,17 +64,6 @@ func main() {
 	latitude, _ := flags.Float64Var(fs, "latitude")
 	longitude, _ := flags.Float64Var(fs, "longitude")
 
-	geometries, _ := flags.StringVar(fs, "geometries")
-
-	alt_geoms, _ := flags.MultiStringVar(fs, "alternate-geometry")
-	is_current, _ := flags.MultiStringVar(fs, "is-current")
-	is_ceased, _ := flags.MultiStringVar(fs, "is-ceased")
-	is_deprecated, _ := flags.MultiStringVar(fs, "is-deprecated")
-	is_superseded, _ := flags.MultiStringVar(fs, "is-superseded")
-	is_superseding, _ := flags.MultiStringVar(fs, "is-superseding")
-
-	pts, _ := flags.MultiStringVar(fs, "placetype")
-
 	ctx := context.Background()
 
 	db, err := database.NewSpatialDatabase(ctx, database_uri)
@@ -122,40 +108,7 @@ func main() {
 		log.Fatalf("Failed to create new coordinate, %v", err)
 	}
 
-	// START OF put me in a WithFlagSet(fs) function
-
-	q := url.Values{}
-	q.Set("geometries", geometries)
-
-	for _, v := range alt_geoms {
-		q.Add("alternate_geometry", v)
-	}
-
-	for _, v := range pts {
-		q.Add("placetype", v)
-	}
-
-	for _, v := range is_current {
-		q.Add("is_current", v)
-	}
-
-	for _, v := range is_ceased {
-		q.Add("is_ceased", v)
-	}
-
-	for _, v := range is_deprecated {
-		q.Add("is_deprecated", v)
-	}
-
-	for _, v := range is_superseded {
-		q.Add("is_superseded", v)
-	}
-
-	for _, v := range is_superseding {
-		q.Add("is_superseding", v)
-	}
-
-	f, err := filter.NewSPRFilterFromQuery(q)
+	f, err := flags.NewSPRFilterFromFlagSet(fs)
 
 	if err != nil {
 		log.Fatalf("Failed to create SPR filter, %v", err)
